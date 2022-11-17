dbafb17c0e981413400d6e242a2248709b975fe7
FahadRizki <114449269+FahadRizki@users.noreply.github.com>
2022-11-17 13:50:02 +0700

    Add files via upload

---
+++ Com/lingkaran/Main.java
@@ -0,0 +1,14 @@
+
+package Com.lingkaran;
+
+import Com.lingkaran.*;
+
+public class Main {
+      public static void main(String[] args) {
+        Lingkaran circle_satu = new SetLingkaran();
+        LuasSetengah luassatu = new LuasSetengah(circle_satu);
+        
+        luassatu.hitung();
+        luassatu.tampilkan();
+    }
+}
